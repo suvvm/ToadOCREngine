8bb3f8c5943777133b8596d295a064bca1b26365
suvvm <suvvm@foxmail.com>
2021-03-17 16:37:04 +0800

    copy bootstrap.sh when build

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ if [ -n "$GOPATH" ];then
   mkdir -p ./output/bin  # 创建二进制文件存放目录
   cp -r ./resources ./output/resources
   go build -o ./output/bin/${RUN_NAME}
+  cp ./output/resources/script/bootstrap.sh ./output/bootstrap.sh
+  chmod +x ./output/bootstrap.sh
 else
 	echo "GOPATH is needed!"
 fi
