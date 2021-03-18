9e9ef583469e0ae4cd810ffbf07cd4fc42e6881c
suvvm <suvvm@foxmail.com>
2021-03-18 18:06:33 +0800

    create function to reverse pixel weight, transform images and labels to gorgonia.tensor

--- utils/mnist.go
+++ utils/mnist.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/binary"
+	"gorgonia.org/tensor"
 	"io"
 	"os"
 )
@@ -110,8 +111,66 @@ func ReadImageFile(reader io.Reader) ([]RawImage, error) {
 //	float64 // 缩放后的浮点灰度值
 func PixelWeight(px byte) float64 {
 	pixelVal := (float64(px) / pixelRange * 0.999) + 0.001
-	if pixelVal == 1.0 {
+	if pixelVal == 1.0 {	// 如果缩放后的值为1.0时，为了数学性能的表现稳定，将其记为0.999
 		return 0.999
 	}
 	return pixelVal
+}
+
+// ReversePixelWeight 像素灰度反向放大函数
+// 将缩放后的像素灰度浮点恢复为8位字节灰度值
+//
+// 入参
+//	px float64	// 浮点灰度值
+//
+// 返回
+//	byte		// 8位字节灰度值
+func ReversePixelWeight(px float64) byte {
+	return byte((px - 0.001) / 0.999 * pixelRange)
+}
+
+// PrepareX 将图像数组转化为tensor存储的矩阵
+// tensor设计格式详见 https://github.com/gorgonia/tensor#design-of-dense
+//
+// 入参
+//	images []RawImage	// 完成初始化的mnist图像数组
+//
+// 返回
+//	tensor.Tensor	// tensor矩阵
+func PrepareX(images []RawImage) tensor.Tensor {
+	rows := len(images)	// 矩阵宽度
+	cols := len(images[0])	// 矩阵高度
+	// 创建矩阵的支撑平面切片，tensor会复用当前切片的空间
+	supportSlice := make([]float64, 0, rows * cols)
+	for i := 0; i < rows; i++ {	// 复制缩放后的像素切片进入矩阵平面切片
+		for j := 0; j < len(images[i]); j++ {
+			supportSlice = append(supportSlice, PixelWeight(images[i][j]))
+		}
+	}
+	// 返回rows * cols的tensor矩阵
+	return tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(supportSlice))
+}
+
+// PrepareY 将标签数组转化为tensor存储的矩阵
+//
+// 入参
+//	labels []Label	// 完成初始化的mnist标签数组
+//
+// 返回
+//	tensor.Tensor	// tensor矩阵
+func PrepareY(labels []Label) tensor.Tensor {
+	rows := len(labels)	// 矩阵宽度
+	cols := numLabels	// 矩阵高度
+	supportSlice := make([]float64, 0, rows * cols)	// 创建矩阵的支撑平面切片
+	for i := 0; i < rows; i++ {	// 复制缩放后的像素切片进入矩阵平面切片
+		for j := 0; j < numLabels; j++ {
+			if j == int(labels[i]) {
+				supportSlice = append(supportSlice, 1)
+			} else {
+				supportSlice = append(supportSlice, 0)
+			}
+		}
+	}
+	// 返回rows * cols的tensor矩阵
+	return tensor.New(tensor.WithShape(rows, cols), tensor.WithBacking(supportSlice))
 }
