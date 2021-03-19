3abe0880ea8f308115f50c6193b31fecd904a674
suvvm <suvvm@foxmail.com>
2021-03-19 14:23:15 +0800

    create test image

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"suvvm.work/toad_ocr_engine/utils"
 )
@@ -16,13 +17,16 @@ func main() {
 
 	reader, err = os.Open("resources/mnist/train-labels-idx1-ubyte")
 	if err != nil {
-		fmt.Errorf("err:%s", err)
-		panic(err)
+		log.Fatalf("err:%v", err)
 	}
 	labels, err := utils.ReadLabelFile(reader)
 	if err != nil {
-		fmt.Errorf("err:%s", err)
-		panic(err)
+		log.Fatalf("err:%v", err)
 	}
-	fmt.Printf("number of imgs:%d, number of labs:%d", len(images), len(labels))
+	log.Printf("number of imgs:%d, number of labs:%d", len(images), len(labels))
+
+	data := utils.PrepareX(images)
+	if err = utils.Visualize(data, 10, 10, "image.png"); err != nil {
+		log.Fatalf("visualize error:%v", err)
+	}
 }
